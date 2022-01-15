36fa88ae7b085a1ed21c0abf231e287f1971a71b
lucas-ssv <lucas_99silva@hotmail.com>
2022-01-15 19:09:11 -0300

    feat: working with strings and numbers

---
+++ capitulo01/demo02-strings-e-numbers.js
@@ -0,0 +1,15 @@
+let salary = 1000
+let mySalary = "2000.14"
+let mySalaryCorrect = Number(mySalary)
+
+console.log(salary + mySalaryCorrect)
+console.log(typeof salary, salary)
+console.log(typeof mySalary, mySalary)
+
+console.log(isNaN(mySalaryCorrect))
+
+let myString = 'Hello World!'
+let myOtherString = 'Hello World2'
+let myStringWithVariables = `${myString}. AE!`
+
+console.log(myStringWithVariables)
