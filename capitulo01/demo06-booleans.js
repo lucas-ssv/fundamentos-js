4e7e3c57ea7c2482797e92fd36f630acbf3c35f3
lucas-ssv <lucas_99silva@hotmail.com>
2022-01-15 19:36:20 -0300

    feat: add variables with type boolean

---
+++ capitulo01/demo06-booleans.js
@@ -0,0 +1,16 @@
+const canDrive = true
+if (canDrive) {
+  console.log('pode dirigir')
+}
+
+const accountBalance = 0
+if (!accountBalance) {
+  console.log('sem saldo')
+}
+
+const boolWithString = 'Eaeeee!'
+if (!!boolWithString) {
+  console.log('blzzz')
+}
+
+console.log(!(!!boolWithString))
