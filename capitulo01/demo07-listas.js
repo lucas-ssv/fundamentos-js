1bf47d6ed9f2093ad8509cc2cce4fed47e79adf9
lucas-ssv <lucas_99silva@hotmail.com>
2022-01-15 20:46:16 -0300

    feat: working with arrays

---
+++ capitulo01/demo07-listas.js
@@ -0,0 +1,45 @@
+const myList = []
+const myTasksList = [
+  'mandar email',
+  'colocar comida para o dog',
+  'limpar o quarto'
+]
+
+// console.log(myTasksList[1])
+// console.log(myTasksList[2])
+// console.log(myTasksList[4])
+
+// console.log(myTasksList.length)
+
+// adicionar item na lista
+// myTasksList.push('comer comida japonesa')
+// console.log(myTasksList)
+
+// remover ultimo item da lista
+// myTasksList.pop()
+// console.log(myTasksList)
+
+// remover primeiro item da lista
+// myTasksList.shift()
+// console.log(myTasksList)
+
+// remover item específico
+// console.log(myTasksList)
+// myTasksList.splice(0, 1)
+// console.log(myTasksList)
+
+const items = [
+  1,
+  'computador',
+  0.22
+]
+// console.log(typeof items)
+// console.log(Array.isArray(items))
+
+const numbers = [30, 20, 10, 5]
+// console.log(numbers.sort())
+// console.log(numbers.concat(['a', 'b', 'c', 'd']))
+// console.log(numbers.join('-'))
+// console.log(numbers.indexOf(30))
+// const index = numbers.indexOf(10)
+// console.log(numbers[index])
