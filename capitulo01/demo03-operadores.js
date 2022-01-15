35eb69e822374e3f6c37bb1cea664c969cc54c08
lucas-ssv <lucas_99silva@hotmail.com>
2022-01-15 19:19:08 -0300

    feat: knowing the operators of JS

---
+++ capitulo01/demo03-operadores.js
@@ -0,0 +1,8 @@
+console.log("1+1=", 1 + 1)
+console.log("2-4=", 2 - 4)
+console.log("2*2=", 2 * 2)
+console.log("4/2=", 4 / 2)
+
+console.log("1>2=", 1 > 2)
+console.log("4===2=", 4 === 2)
+console.log("5!==5=", 5 !== 5)
