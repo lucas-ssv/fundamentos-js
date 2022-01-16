57f485d64b1a66ff70001fdacc10f4fc2c6403cd
lucas-ssv <lucas_99silva@hotmail.com>
2022-01-15 21:17:34 -0300

    feat: working with global objects

---
+++ capitulo01/demo10-objetos-globais.js
@@ -0,0 +1,7 @@
+console.log('keys', Object.keys({ name: 'Lucas' }))
+console.log('values', Object.values({ name: 'Lucas' }))
+console.log('date', Date.now())
+console.log('random', Math.random())
+
+// global
+console.log(global)
