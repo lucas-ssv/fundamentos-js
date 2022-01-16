dd0563f45088dee0d20339b12ce1719dd7f387ab
lucas-ssv <lucas_99silva@hotmail.com>
2022-01-15 21:27:23 -0300

    feat: working with dates

---
+++ capitulo01/demo11-date.js
@@ -0,0 +1,10 @@
+// meses começam do zero
+const birthDate = new Date(2022, 0, 20)
+console.log(birthDate)
+
+const firstDateJs = new Date(0)
+console.log(firstDateJs.getTime())
+
+const today = new Date()
+console.log(String(today))
+console.log(today.toISOString())
